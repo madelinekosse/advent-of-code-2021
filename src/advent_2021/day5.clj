8b670d6d586702580115cb109059184f32fc2517
Madeline Kosse <madelinekosse94@gmail.com>
2021-12-05 12:23:15 +0000

    Refactor day5

--- src/advent_2021/day5.clj
+++ src/advent_2021/day5.clj
@@ -13,57 +13,22 @@
 (defn input-rows->lines [rows]
   (map input-row->line rows))
 
-(defn- horizontal? [[from to]]
-  (let [{from-y :y} from
-        {to-y :y} to]
-    (= from-y to-y)))
-
-(defn- vertical? [[from to]]
-  (let [{from-x :x} from
-        {to-x :x} to]
-    (= from-x to-x)))
-
-(defn- diagonal-45? [coords]
-  (let [[x1 x2] (sort (map :x coords))
-        [y1 y2] (sort (map :y coords))]
-    (= (- x2 x1)
-       (- y2 y1))))
-
 (defn filter-remove-diagonals [lines]
-  (filter #(or (vertical? %) (horizontal? %)) lines))
+  (filter
+   (fn [[from to]]
+     (or (= (:x from) (:x to)) (= (:y from) (:y to))))
+   lines))
 
-(defn- points-crossed-horiz [[from to]]
-  (let [{x1 :x y :y} from
-        {x2 :x} to
-        from-x (min x1 x2)
-        to-x (max x1 x2)]
-    (map (fn [x] {:x x :y y})
-         (range from-x (inc to-x)))))
-
-(defn- points-crossed-vert [[from to]]
-  (let [{x :x y1 :y} from
-        {y2 :y} to
-        from-y (min y1 y2)
-        to-y (max y1 y2)]
-    (map (fn [y] {:x x :y y})
-         (range from-y (inc to-y)))))
-
-(defn- points-crossed-diag [[from to]]
+(defn points-crossed [[from to]]
   (let [{x1 :x y1 :y} from
         {x2 :x y2 :y} to
-        x-direction (if (< x1 x2) inc dec)
-        y-direction (if (< y1 y2) inc dec)]
+        x-direction (cond (< x1 x2) inc (> x1 x2) dec :else identity)
+        y-direction (cond (< y1 y2) inc (> y1 y2) dec :else identity)]
     (loop [points [from]]
       (let [{last-x :x last-y :y :as last-point} (last points)]
         (if (= last-point to)
           points
           (recur (conj points {:x (x-direction last-x) :y (y-direction last-y)})))))))
-
-(defn points-crossed [line]
-  (cond
-    (vertical? line) (points-crossed-vert line)
-    (horizontal? line) (points-crossed-horiz line)
-    (diagonal-45? line) (points-crossed-diag line)))
 
 (defn- line-count-by-point [lines]
   (let [points-crossed (->> lines
@@ -83,13 +48,15 @@
        (filter #(>= % 2))
        count))
 
+(defn- count-intersections-from-input [input-lines exclude-diagonals?]
+  (let [filter-fn (if exclude-diagonals? filter-remove-diagonals identity)]
+    (-> input-lines
+        input-rows->lines
+        filter-fn
+        count-intersections)))
+
 (defn run-part-1 [input-lines]
-  (-> input-lines
-      input-rows->lines
-      filter-remove-diagonals
-      count-intersections))
+  (count-intersections-from-input input-lines true))
 
 (defn run-part-2 [input-lines]
-  (-> input-lines
-      input-rows->lines
-      count-intersections))
+  (count-intersections-from-input input-lines false))
